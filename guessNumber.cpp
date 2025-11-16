84a6e48958b94762c06fabbbe26d3e7a23a2630c
Cheng-0316 <Cheng_0316@outlook.com>
2025-11-16 20:54:48 +0800

    Add files via upload

--- guessNumber.cpp
+++ guessNumber.cpp
@@ -5,9 +5,169 @@
 #include <string>
 
 #include "guessNumber.hpp"
-#include "generalFunctions.hpp"
+#include "nlohmann/json.hpp"
 
-void guessNumber::SpecialInputJudgement(std::string input)
+bool guessNumber_config::ConfigFileExists()
+{
+    //版本限制
+    //直接返回，表示该版本无配置
+    return true;
+    if (!std::filesystem::exists("config.json"))
+    {
+        // 文件不存在就创建
+        Println("文件config.json不存在，正在创建中……");
+        if (FileCreate("config.json"))
+        {
+            Println("文件config.json创建成功！");
+            Println("正在写入默认配置……");
+            nlohmann::json defaultConfig;
+            defaultConfig["version"] = GAME_VERSION;
+            std::ofstream configFile("config.json");
+            configFile << defaultConfig.dump(4) << std::endl;
+            Println("默认配置写入完成。");
+        }
+        else
+        {
+            //没有且无法创建，返回错误
+            Println("无法创建文件config.json！");
+            return false;
+        }
+    }
+    //存在或创建成功，返回正常
+    return true;
+}
+
+void guessNumber_config::LoadConfigFile()
+{
+    if (!ConfigFileExists())
+    {
+        // 配置文件无法创建且不存在
+        // 退出游戏时不进行保存
+        Println("游戏将使用默认配置，并不进行保存。");
+        configSaving = false;
+        return;
+    }
+    // 目前无可读取配置
+    Println("版本限制，配置不进行加载。");
+    configSaving = false;
+    return;
+    // 读取配置
+    Println("加载游戏配置中……");
+    ;
+    Println("加载配置完成。");
+}
+
+void guessNumber_config::SaveConfig()
+{
+    nlohmann::ordered_json config;
+    config["version"] = GAME_VERSION;
+    std::ofstream configFile("config.json");
+    configFile << config.dump(4) << std::endl;
+}
+
+bool guessNumber_archive::ArchiveFileExists()
+{
+    if (!std::filesystem::exists("archiveFile.json"))
+    {
+        // 文件不存在就创建
+        Println("文件archiveFile.json不存在，正在创建中……");
+        if (FileCreate("archiveFile.json"))
+        {
+            Println("文件archiveFile.json创建成功！");
+            Println("正在初始化存档中……");
+            nlohmann::ordered_json archive;
+            archive["version"] = GAME_VERSION;
+            archive["wealth"] = 0;
+            std::ofstream archiveFile("archiveFile.json");
+            archiveFile << archive.dump(4) << std::endl;
+            Println("存档初始化完成。");
+        }
+        else
+        {
+            Println("无法创建文件archiveFile.json！");
+            return false;
+        }
+    }
+    return true;
+}
+
+void guessNumber_archive::LoadArchiveFile()
+{
+    // 如果存档文件不存在且无法创建
+    if (!ArchiveFileExists())
+    {
+        // 退出游戏时不进行保存
+        Println("游戏将使用空存档，且不进行保存。");
+        archiveSaving = false;
+        return;
+    }
+    // 读取存档
+    Println("加载存档中……");
+    std::ifstream archiveFile("archiveFile.json");
+    // 无法打开存档就取消读档，且游戏结束后不会保存
+    if (!archiveFile.is_open())
+    {
+        std::cerr << "文件archiveFile.json未找到或无法打开！" << std::endl;
+        archiveSaving = false;
+        return;
+    }
+    nlohmann::ordered_json archive;
+    try
+    {
+        archive = nlohmann::ordered_json::parse(archiveFile);
+    }
+    catch (const std::exception &e)
+    {
+        // 遇到错误的格式也取消读档，但游戏结束后会保存
+        Println("存档格式错误。");
+        std::cerr << e.what() << std::endl;
+        Println("取消读档。");
+        archiveSaving = true;
+        return;
+    }
+    archiveFile.close();
+    // 读取财富值
+    // 还需判断值是否存在！！！
+    wealth = archive["wealth"];
+    Println("加载存档完成。");
+    archiveSaving = true;
+}
+
+void guessNumber_archive::SaveArchive()
+{
+    nlohmann::ordered_json archive;
+    archive["version"] = GAME_VERSION;
+    archive["wealth"] = wealth;
+    std::ofstream archiveFile("archiveFile.json");
+    archiveFile << archive.dump(4) << std::endl;
+}
+
+
+void guessNumber::SaveAll()
+{
+    if (ganeConfig.configSaving == true)
+    {
+        Println("保存配置中……");
+        ganeConfig.SaveConfig();
+        Println("配置保存完毕。");
+    }
+    else
+    {
+        Println("版本限制，配置不进行保存。");
+    }
+    if (gameArchive.archiveSaving == true)
+    {
+        Println("保存存档中……");
+        gameArchive.SaveArchive();
+        Println("存档保存完毕。");
+    }
+    else
+    {
+        Println("存档无法保存。");
+    }
+}
+
+void guessNumber::SpecialInput(std::string input)
 {
     if (input == "exit" || input == "quit")
     {
@@ -17,19 +177,27 @@ void guessNumber::SpecialInputJudgement(std::string input)
     {
         gameStatus = GAME_RESTART;
     }
+
+    // 测试代码
+    if (input == "wealth_increase")
+    {
+        gameArchive.wealth += 100;
+        gameStatus = 3;
+        Println("你的财富：" + std::to_string(gameArchive.wealth));
+    }
 }
 
-int guessNumber::InputStringtoInt(std::string input)
+int guessNumber::StringtoInt(std::string input)
 {
     int number = 0;
     try
     {
         number = std::stoi(input);
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
-        Print("输入不可识别，\n");
+        Println("输入不可识别，");
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         gameStatus = INPUT_UNAVAILABLE;
@@ -41,7 +209,7 @@ void guessNumber::ChooseDifficulty()
 {
     std::string input;
     do
-    {   
+    {
         gameStatus = GAME_CONTINUE;
         std::cout << "请选择难度:" << std::endl;
         std::cout << "1：简单（1-" << EASY_MAX_NUMBER << "，无猜测次数限制）" << std::endl;
@@ -50,32 +218,32 @@ void guessNumber::ChooseDifficulty()
         std::cout << "其他数字：自定义" << std::endl;
         std::cout << "选择难度：" << std::flush;
         std::cin >> input;
-        difficulty = InputStringtoInt(input);
+        recoveryDifficulty = StringtoInt(input);
     } while (gameStatus != GAME_CONTINUE);
 }
 
 void guessNumber::InbuiltDifficultyFilling()
 {
-    switch (difficulty)
+    switch (recoveryDifficulty)
     {
     case 1:
         recoveryMax = EASY_MAX_NUMBER;
-        recoveryGuessLimit = EASY_LIMITED_TIMES;
+        recoveryLimitedTimes = EASY_LIMITED_TIMES;
         break;
     case 2:
         recoveryMax = NORMAL_MAX_NUMBER;
-        recoveryGuessLimit = NORMAL_LIMITED_TIMES;
+        recoveryLimitedTimes = NORMAL_LIMITED_TIMES;
         break;
     case 3:
         recoveryMax = HARD_MAX_NUMBER;
-        recoveryGuessLimit = HARD_LIMITED_TIMES;
+        recoveryLimitedTimes = HARD_LIMITED_TIMES;
         break;
     default:
         break;
     }
 }
 
-void guessNumber::SetUserDefinedMaxRandom()
+void guessNumber::SetRandomLimit_UserDefined()
 {
     std::string input;
     do
@@ -83,7 +251,7 @@ void guessNumber::SetUserDefinedMaxRandom()
         gameStatus = GAME_CONTINUE;
         std::cout << "请输入自定义最大猜测值：" << std::flush;
         std::cin >> input;
-        recoveryMax = InputStringtoInt(input);
+        recoveryMax = StringtoInt(input);
         if (recoveryMax < 2)
         {
             std::cout << "请确保输入值大于1，" << std::endl;
@@ -92,7 +260,7 @@ void guessNumber::SetUserDefinedMaxRandom()
     } while (gameStatus != GAME_CONTINUE);
 }
 
-void guessNumber::SetUserDefinedMaxGuessLimit()
+void guessNumber::SetLimitedTimes_UserDefined()
 {
     std::string input;
     do
@@ -101,69 +269,77 @@ void guessNumber::SetUserDefinedMaxGuessLimit()
         std::cout << "（输入负数代表无限制）" << std::endl;
         std::cout << "请输入自定义最大猜测次数：" << std::flush;
         std::cin >> input;
-        recoveryGuessLimit = InputStringtoInt(input);
+        recoveryLimitedTimes = StringtoInt(input);
     } while (gameStatus != GAME_CONTINUE);
-}
-
-void guessNumber::ResetDifficulty()
-{
-    minGuessLimit = 0;
-    maxGuessLimit = recoveryMax;
-    limitedGuessTimes = recoveryGuessLimit;
 }
 
 void guessNumber::SetDifficulty()
 {
     std::string input;
     ChooseDifficulty();
-    if (difficulty < 1 || difficulty > 3)
+    //所有值为先保存至恢复数据中，再填充到游戏数据中
+    //如果用户自定义难度
+    if (recoveryDifficulty < 1 || recoveryDifficulty > 3)
     {
-        SetUserDefinedMaxRandom();
-        SetUserDefinedMaxGuessLimit();
+        //让用户自己输入随机数范围和限制猜测次数
+        SetRandomLimit_UserDefined();
+        SetLimitedTimes_UserDefined();
     }
-    InbuiltDifficultyFilling();
+    else
+    {
+        //否则自动填充难度相关恢复数据
+        InbuiltDifficultyFilling();
+    }
     recoveryMax++;
     //此处的重置难度是为了自动填充
     ResetDifficulty();
+}
+
+void guessNumber::ResetDifficulty()
+{
+    minGuess = 0;
+    maxGuess = recoveryMax;
+    limitedTimes = recoveryLimitedTimes;
+    difficulty = recoveryDifficulty;
 }
 
 void guessNumber::GenerateRandom()
 {
     std::random_device rd;
     std::mt19937 gen(rd());
-    //减一确保生成的随机数在最大值以内，防止其等于最大值，导致用户无法猜到
-    std::uniform_int_distribution<> dis(1, maxGuessLimit - 1);
-    randomNumber = dis(gen);
+    // 减一确保生成的随机数在最大值以内，防止其等于最大值，导致用户无法猜到
+    std::uniform_int_distribution<> dis(1, maxGuess - 1);
+    random = dis(gen);
     // std::cout << "（调试信息：随机数为 " << randomNumber << "）" << std::endl;
 }
 
-bool guessNumber::RangeJudgement(int inputNumber)
+bool guessNumber::OutOfRange(int inputNumber)
 {
-    //判断用户输入的值是否在范围内
-    if (inputNumber <= minGuessLimit || inputNumber >= maxGuessLimit)
+    // 判断用户输入的值是否在范围内
+    if (inputNumber <= minGuess || inputNumber >= maxGuess)
     {
         std::cout << "输入的数字不属于限制范围，" << std::endl;
-        return false;
+        return true;
     }
-    return true;
+    return false;
 }
 
 bool guessNumber::SizeJudgement()
 {
-    if (guessNumber == randomNumber)
+    if (guess == random)
     {
         return true;
     }
-    if (guessNumber < randomNumber)
+    if (guess < random)
     {
         std::cout << "太小了！" << std::endl;
-        minGuessLimit = guessNumber;
+        minGuess = guess;
         gameStatus = GAME_CONTINUE;
     }
-    if (guessNumber > randomNumber)
+    if (guess > random)
     {
         std::cout << "太大了！" << std::endl;
-        maxGuessLimit = guessNumber;
+        maxGuess = guess;
         gameStatus = GAME_CONTINUE;
     }
     return false;
@@ -171,11 +347,12 @@ bool guessNumber::SizeJudgement()
 
 void guessNumber::DealPlayerGuess()
 {
-    if (RangeJudgement(guessNumber))
+    //如果输入的数字在范围内
+    if (!OutOfRange(guess))
     {
-        if (limitedGuessTimes > 0)
+        if (limitedTimes > 0)
         {
-            limitedGuessTimes--;
+            limitedTimes--;
         }
         if (SizeJudgement())
         {
@@ -184,9 +361,9 @@ void guessNumber::DealPlayerGuess()
             gameStatus = SINGLE_GAME_END_NORMALLY;
             return;
         }
-        if (limitedGuessTimes == 0 || minGuessLimit + 2 == maxGuessLimit)
+        if (limitedTimes == 0 || minGuess + 2 == maxGuess)
         {
-            std::cout << "你已经没有机会了，正确答案是：" << randomNumber << std::endl;
+            std::cout << "你已经没有机会了，正确答案是：" << random << std::endl;
             gameStatus = SINGLE_GAME_END_NORMALLY;
         }
     }
@@ -194,76 +371,10 @@ void guessNumber::DealPlayerGuess()
 
 void guessNumber::WealthIncrease()
 {
-    wealth += 10;
-    if (limitedGuessTimes > 0)
+    gameArchive.wealth += 10;
+    if (limitedTimes > 0)
     {
-        wealth += limitedGuessTimes * 2;
-    }
-}
-
-void guessNumber::SinglePlay()
-{
-    GenerateRandom();
-    std::string input;
-    do
-    {
-        gameStatus = GAME_CONTINUE;
-        std::cout << "（输入exit或quit退出程序，输入restart或res重新开始游戏）" << std::endl;
-        if (limitedGuessTimes < 0)
-        {
-            std::cout << "无限制猜测次数，" << std::flush;
-        }
-        else
-        {
-            std::cout << "还剩" << limitedGuessTimes << "猜测次数，" << std::flush;
-        }
-        std::cout << "输入一个从" << minGuessLimit << "到" << maxGuessLimit << "之内的整数：" << std::flush;
-        std::cin >> input;
-        SpecialInputJudgement(input);
-        if (gameStatus == WHOLE_GAME_END_NORMALLY || gameStatus == GAME_RESTART)
-        {
-            break;
-        }
-        guessNumber = InputStringtoInt(input);
-        if (gameStatus == INPUT_UNAVAILABLE)
-        {
-            continue;
-        }
-        DealPlayerGuess();
-        if (gameStatus == INPUT_UNAVAILABLE)
-        {
-            continue;
-        }
-    } while (gameStatus != SINGLE_GAME_END_NORMALLY);
-}
-
-void guessNumber::SingleEndDealing()
-{
-    char choice;
-    switch (gameStatus)
-    {
-    case SINGLE_GAME_END_NORMALLY:
-        std::cout << "是否继续游戏？(y/n)" << std::endl;
-        std::cin >> choice;
-        if (choice == 'y' || choice == 'Y')
-        {
-            gameStatus = GAME_RESTART;
-        }
-        else
-        {
-            gameStatus = WHOLE_GAME_END_NORMALLY;
-        }
-        break;
-    /*
-    case GAME_RESTART:
-        break;
-    case GAME_CONTINUE:
-        break;
-    case WHOLE_GAME_END_NORMALLY:
-        break;
-    */
-    default:
-        break;
+        gameArchive.wealth += limitedTimes * 2;
     }
 }
 
@@ -276,42 +387,105 @@ void guessNumber::ComputerGuessDemo()
     GenerateRandom();
     do
     {
-        std::cout << "当前范围：(" << minGuessLimit << "," << maxGuessLimit << ")" << std::endl;
-        guessNumber = (minGuessLimit + maxGuessLimit) / 2;
+        std::cout << "当前范围：(" << minGuess << "," << maxGuess << ")" << std::endl;
+        guess = (minGuess + maxGuess) / 2;
         std::cout << "电脑二分搜索：" << std::endl;
-        std::cout << "(" << minGuessLimit << " + " << maxGuessLimit << ") ÷ 2 = " << guessNumber << std::endl;
+        std::cout << "(" << minGuess << " + " << maxGuess << ") ÷ 2 = " << guess << std::endl;
         SizeJudgement();
-    } while (guessNumber != randomNumber);
-    std::cout << "电脑二分法搜索演示完毕，随机数为：" << guessNumber << "。" << std::endl;
+    } while (guess != random);
+    std::cout << "电脑二分法搜索演示完毕，随机数为：" << guess << std::endl;
+}
+
+void guessNumber::PrintClue()
+{
+    Println("（输入exit或quit退出程序，输入restart或res重新开始游戏）");
+    if (limitedTimes < 0)
+    {
+        Println("无限制猜测次数。");
+    }
+    else
+    {
+        Println("还剩" + std::to_string(limitedTimes) + "猜测次数。");
+    }
+    Print("输入一个大于" + std::to_string(minGuess));
+    Print("且小于" + std::to_string(maxGuess) + "的整数：");
+}
+
+void guessNumber::SinglePlay()
+{
+    std::cout << "你的财富：" << gameArchive.wealth << std::endl;
+    GenerateRandom();
+    std::string input;
+    do
+    {
+        gameStatus = GAME_CONTINUE;
+        PrintClue();
+        std::cin >> input;
+        SpecialInput(input);
+        if (gameStatus == WHOLE_GAME_END_NORMALLY || gameStatus == GAME_RESTART)
+        {
+            break;
+        }
+
+        // 测试代码
+        if (gameStatus == 3)
+        {
+            continue;
+        }
+
+        guess = StringtoInt(input);
+        if (gameStatus == INPUT_UNAVAILABLE)
+        {
+            continue;
+        }
+        DealPlayerGuess();
+        if (gameStatus == INPUT_UNAVAILABLE)
+        {
+            continue;
+        }
+    } while (gameStatus != SINGLE_GAME_END_NORMALLY);
+    SingleEnd();
+}
+
+void guessNumber::SingleEnd()
+{
+    char choice;
+    //处理单次游戏正常结束
+    if (gameStatus == SINGLE_GAME_END_NORMALLY)
+    {
+        Println("是否继续游戏？(y/n)");
+        std::cin >> choice;
+        if (choice == 'y' || choice == 'Y')
+        {
+            gameStatus = GAME_RESTART;
+        }
+        else
+        {
+            gameStatus = WHOLE_GAME_END_NORMALLY;
+        }
+    }
 }
 
 void guessNumber::RepeatedlyPlay()
 {
-    if (!FileCreat("config.ini"))
-    {
-        std::cerr << "文件" <<"savedata.txt" << "创建失败，游戏将使用默认配置。" << std::endl;
-    }
-    if (!FileCreat("savedata.txt"))
-    {
-        std::cerr << "文件" <<"savedata.txt" << "创建失败，游戏记录功能将无法使用。" << std::endl;
-    }
-    char choice;
-    std::cout << "游戏开始！版本：" << GAME_VERSION << std::endl;
+    Print("游戏版本：");
+    Println(GAME_VERSION);
+    ganeConfig.LoadConfigFile();
+    gameArchive.LoadArchiveFile();
+    Println("游戏开始！\n你的财富：" + std::to_string(gameArchive.wealth));
     SetDifficulty();
+    char choice;
     while (gameStatus != WHOLE_GAME_END_NORMALLY)
     {
-        std::cout << "是否观看演示？(y/n)" << std::endl;
+        Println("是否观看演示？(y/n)");
         std::cin >> choice;
         if (choice == 'y' || choice == 'Y')
         {
             ComputerGuessDemo();
-            std::cout << "按回车健结束……" << std::endl;
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cin.get();
+            ResetDifficulty();
         }
-        std::cout << "你的金钱目前为：" << wealth << std::endl;
         SinglePlay();
-        SingleEndDealing();
+        /*
         if (gameStatus == -1)
         {
             std::cout << "游戏错误地结束。" << std::endl;
@@ -319,6 +493,7 @@ void guessNumber::RepeatedlyPlay()
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             return;
         }
+        */
         if (gameStatus == GAME_RESTART)
         {
             std::cout << "游戏重新开始！" << std::endl;
@@ -334,8 +509,29 @@ void guessNumber::RepeatedlyPlay()
             }
         }
     }
-    std::cout << "感谢参与游戏！" << std::endl;
-    std::cout << "按回车健退出……" << std::endl;
+    SaveAll();
+    Println("感谢参与游戏！\n回车健退出……");
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cin.get();
+}
+
+bool FileCreate(std::string fileName)
+{
+    std::ofstream file(fileName);
+    if (file)
+    {
+        file.close();
+        return true;
+    }
+    return false;
+}
+
+void Print(std::string message)
+{
+    std::cout << message << std::flush;
+}
+
+void Println(std::string message)
+{
+    std::cout << message << std::endl;
 }
--- guessNumber.hpp
+++ guessNumber.hpp
@@ -4,7 +4,9 @@
 #include <string>
 #include <vector>
 
-#define GAME_VERSION "2.1.2"
+#include "nlohmann/json.hpp"
+
+#define GAME_VERSION "2.3.0"
 
 #define INPUT_UNAVAILABLE 1
 #define GAME_ERROR 2
@@ -22,63 +24,112 @@
 #define NORMAL_LIMITED_TIMES 7
 #define HARD_LIMITED_TIMES 8
 
-//特殊输入列表
-const std::vector<std::string> specialInput_exit =
-{"exit", "quit",};
-//特殊输入列表
-const std::vector<std::string> specialInput_restart =
-{"res", "restart",};
+//多人模式（网络对战）（玩家AI对战）
+//单次猜测限时、单次游戏整体限时
+//动态难度、范围、复合判定
+//体力值系统、商店系统、成就系统
+//动态反馈、主题切换
+//配置文件、支持多语言
+//排行榜：本地、网络
+//数据统计：成功率、平均猜测次数
+
+//电脑二分法查找演示（）
+
+//进行存档
+
+// //特殊输入列表
+// const std::vector<std::string> specialInput_exit =
+// {"exit", "quit",};
+// //特殊输入列表
+// const std::vector<std::string> specialInput_restart =
+// {"res", "restart",};
+
+struct guessNumber_config
+{
+    //游戏结束时，决定是否保存
+    bool configSaving;
+    //配置数据
+    std::string languageFilePath;
+    //判断配置文件是否存在
+    bool ConfigFileExists();
+    //读取配置文件
+    void LoadConfigFile();
+    //保存配置
+    void SaveConfig();
+};
+
+struct guessNumber_archive
+{
+    //游戏结束时，决定是否保存
+    bool archiveSaving;
+    //存档的财富值
+    int wealth;
+    //判断存档文件是否存在
+    bool ArchiveFileExists();
+    //读取存档文件
+    void LoadArchiveFile();
+    //保存存档
+    void SaveArchive();
+};
 
 class guessNumber
 {
     //保存游戏状态
     int gameStatus;
-    //保存难度相关数据
-    int recoveryGuessLimit, recoveryMax;
+    //保存难度数据（用于恢复）
+    int recoveryDifficulty, recoveryLimitedTimes, recoveryMax;
     //保存当前难度数据
-    int difficulty, limitedGuessTimes, randomNumber;
+    int difficulty, limitedTimes, random;
     //保存当前猜测数据
-    int guessNumber, minGuessLimit, maxGuessLimit;
-    //玩家财富值
-    int wealth;
+    int guess, minGuess, maxGuess;
+    guessNumber_config ganeConfig;
+    guessNumber_archive gameArchive;
     private:
-        //提出特殊输入
-        void SpecialInputJudgement(std::string input);
+        void SaveAll();
+        //判断特殊输入
+        void SpecialInput(std::string input);
         //输入string转int
-        int InputStringtoInt(std::string input);
+        int StringtoInt(std::string input);
         //选择难度
         void ChooseDifficulty();
         //内置难度填充
         void InbuiltDifficultyFilling();
-        //用户自定义最大随机值
-        void SetUserDefinedMaxRandom();
-        //用户自定义最大猜测次数
-        void SetUserDefinedMaxGuessLimit();
-        //重置难度
-        void ResetDifficulty();
+        //自定义最大随机值
+        void SetRandomLimit_UserDefined();
+        //自定义最大猜测次数
+        void SetLimitedTimes_UserDefined();
         //设置难度
         void SetDifficulty();
+        //重置难度相关数据
+        void ResetDifficulty();
         //生成随机数
         void GenerateRandom();
-        //判断用户的输入值是否在规定范围内
-        bool RangeJudgement(int inputNumber);
-        //判断用户的猜测值相对被猜数的大小
+        //判断输入值是否在规定范围内
+        bool OutOfRange(int inputNumber);
+        //判断猜测值相对被猜数的大小
         bool SizeJudgement();
-        //处理用户猜测值
+        //处理猜测值
         void DealPlayerGuess();
-        //
+        //增加财富
         void WealthIncrease();
+        //机器二分法搜索演示
+        void ComputerGuessDemo();
+        //打印猜测线索
+        void PrintClue();
         //进行一次游戏
         void SinglePlay();
         //单次游戏解释处理
-        void SingleEndDealing();
+        void SingleEnd();
     public:
-        //机器二分法搜索演示
-        void ComputerGuessDemo();
         //重复游玩直至玩家自主退出
         void RepeatedlyPlay();
-        //返回当前游戏状态
-        int ReturnGameStatus() { return gameStatus; }
 };
+
+//创建一个文件不管其是否存在
+bool FileCreate(std::string fileName);
+//打印信息
+void Print(std::string message);
+//打印信息（加入换行符）
+void Println(std::string message);
 
 #endif
